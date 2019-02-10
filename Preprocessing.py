3037b7a6b7f6fcdb57ebd07bbc09ffc767a748ac
danielvizcaya <daniel@datagran.io>
2019-02-10 09:01:57 -0500

    [Add] Inital files for preprocessing

---
+++ Preprocessing.py
@@ -0,0 +1,20 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.3'
+#       jupytext_version: 0.8.6
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# # Preprocessing
+#
+# #### The preprocessing will be carried out using three different approaches in order to compare how easy it is to work with each of them. The three tools will be Pandas, pySpark and Dask.
+
+
